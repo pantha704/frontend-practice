5d694765333d11ad0ef95a36c621fb846a6dc61c
pantha704 <prathamjaiswal204@gmail.com>
2024-10-28 17:35:40 +0530

    .

--- parallax/fireship/src/App.jsx
+++ parallax/fireship/src/App.jsx
@@ -1,65 +1,90 @@
-import "./App.css";
+import { useRef } from "react";
+import moon from "./assets/moon.png";
+import land from "./assets/land.png";
+import cat from "./assets/cat.gif";
 
 import { Parallax, ParallaxLayer } from "@react-spring/parallax";
 
 function App() {
+  const ref = useRef();
+
   return (
-    <Parallax pages={3}>
-      <ParallaxLayer
-        offset={0}
-        speed={0.5}
-        style={{
-          backgroundColor: "#805E73",
-        }}
-      />
-      <ParallaxLayer
-        offset={1}
-        speed={0.8}
-        style={{
-          backgroundColor: "#87BCDE",
-        }}
-      />
-      <ParallaxLayer
-        offset={2}
-        speed={1.2}
-        style={{
-          backgroundColor: "#ECEBDB",
-        }}
-      />
-      <ParallaxLayer
-        offset={0}
-        speed={0.3}
-        style={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-        }}
+    <div className="min-h-screen min-w-screen overflow-hidden p-0 m-0 bg-black border-black">
+      <Parallax
+        pages={4}
+        ref={ref}
+        style={{ backgroundColor: "black", padding: 0, margin: 0 }}
       >
-        <h1>Welcome to My Website</h1>
-      </ParallaxLayer>
-      <ParallaxLayer
-        offset={1}
-        speed={0.5}
-        style={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-      >
-        <h1>About Us</h1>
-      </ParallaxLayer>
-      <ParallaxLayer
-        offset={2}
-        speed={0.7}
-        style={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-      >
-        <h1>Contact</h1>
-      </ParallaxLayer>
-    </Parallax>
+        <ParallaxLayer
+          sticky={{ start: 0.9, end: 2.5 }}
+          style={{ textAlign: "center" }}
+        >
+          <img src={cat} />
+        </ParallaxLayer>
+
+        {/* <ParallaxLayer speed={1}>
+            <h2>Welcome to my website</h2>
+        </ParallaxLayer>
+
+        <ParallaxLayer offset={1} speed={0.5}>
+            <h2>Web development is fun!</h2>
+        </ParallaxLayer> */}
+        <div className="h-screen w-full">
+          <ParallaxLayer
+            offset={0}
+            speed={1}
+            factor={2}
+            style={{
+              backgroundImage: `url(${moon})`,
+              backgroundSize: "contain",
+            }}
+          />
+
+          <ParallaxLayer
+            offset={2}
+            speed={1}
+            factor={4}
+            style={{
+              backgroundImage: `url(${land})`,
+              backgroundSize: "cover",
+            }}
+          ></ParallaxLayer>
+          <ParallaxLayer
+            offset={0.2}
+            speed={0.05}
+            onClick={() => ref.current.scrollTo(3)}
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              cursor: "pointer",
+              color: "white",
+              fontSize: "4rem",
+              fontWeight: "bold",
+            }}
+          >
+            <h1>Welcome to my website</h1>
+          </ParallaxLayer>
+
+          <ParallaxLayer
+            offset={3}
+            speed={2}
+            onClick={() => ref.current.scrollTo(0)}
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              cursor: "pointer",
+              color: "white",
+              fontSize: "4rem",
+              fontWeight: "bold",
+            }}
+          >
+            <h2>Hi Mom!</h2>
+          </ParallaxLayer>
+        </div>
+      </Parallax>
+    </div>
   );
 }
 
